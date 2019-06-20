88e7d2ddf98cc92c3c0b466da1f4f72886a69dd1
Jan-Philipp <jpm3492@gmail.com>
2019-06-20 15:35:29 +0200

    added babel

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { quantumReducer, initializeReducers, dispatchToReducer } from './quantum_reducer'
 import { quantumState, setQuantumValue } from './quantum_state'
 
-exports.quantumReducer = quantumReducer
-exports.initializeReducers = initializeReducers
-exports.dispatchToReducer = dispatchToReducer
-exports.quantumState = quantumState
-exports.setQuantumValue = setQuantumValue
+export {
+  quantumState,
+  setQuantumValue,
+  quantumReducer,
+  initializeReducers,
+  dispatchToReducer
+}
